d185dfad6454ea464d99f47168b357927df04729
msafrain <msafrain@gmail.com>
2025-11-19 16:10:42 +0800

    desktop.js

--- desktop.js
+++ desktop.js
@@ -1,6 +1,5 @@
 // desktop.js
 // Window management for mSafrain Desktop UI
-// Simple + stable: numeric IDs only, mobile scroll-to-window
 
 (function () {
   function initDesktopUI($) {
@@ -14,10 +13,6 @@
         windowLeftPos = [],
         id;
 
-      function isMobile() {
-        return window.innerWidth <= 768;
-      }
-
       function adjustFullScreenSize() {
         $("#mSafrain .fullSizeWindow .wincontent")
           .css("width", window.innerWidth - 32)
@@ -26,10 +21,7 @@
 
       function makeWindowActive(thisid) {
         $("#mSafrain .window").each(function () {
-          $(this).css(
-            "z-index",
-            parseInt($(this).css("z-index") || 0, 10) - 1
-          );
+          $(this).css("z-index", parseInt($(this).css("z-index") || 0, 10) - 1);
         });
         $("#window" + thisid).css("z-index", 1000);
         $("#mSafrain .window").removeClass("activeWindow");
@@ -39,31 +31,21 @@
       }
 
       function minimizeWindow(id) {
-        // Remember original position (desktop only)
         windowTopPos[id] = $("#window" + id).css("top");
         windowLeftPos[id] = $("#window" + id).css("left");
 
-        if (isMobile()) {
-          // Mobile: just slide up and mark minimized
-          $("#window" + id).addClass("minimizedWindow").slideUp(200);
-          $("#minimPanel" + id)
-            .addClass("minimizedTab")
-            .removeClass("activeTab");
-        } else {
-          // Desktop: animate to bottom
-          $("#window" + id).animate(
-            {
-              top: 800,
-              left: 0,
-            },
-            200,
-            function () {
-              $("#window" + id).addClass("minimizedWindow");
-              $("#minimPanel" + id).addClass("minimizedTab");
-              $("#minimPanel" + id).removeClass("activeTab");
-            }
-          );
-        }
+        $("#window" + id).animate(
+          {
+            top: 800,
+            left: 0,
+          },
+          200,
+          function () {
+            $("#window" + id).addClass("minimizedWindow");
+            $("#minimPanel" + id).addClass("minimizedTab");
+            $("#minimPanel" + id).removeClass("activeTab");
+          }
+        );
       }
 
       function openWindow(id) {
@@ -84,25 +66,19 @@
       function openMinimized(id) {
         $("#window" + id).removeClass("minimizedWindow");
         $("#minimPanel" + id).removeClass("minimizedTab");
+        makeWindowActive(id);
 
-        if (isMobile()) {
-          $("#window" + id).slideDown(200, function () {
-            makeWindowActive(id);
-          });
-        } else {
-          makeWindowActive(id);
-          $("#window" + id).animate(
-            {
-              top: windowTopPos[id],
-              left: windowLeftPos[id],
-            },
-            200
-          );
-        }
+        $("#window" + id).animate(
+          {
+            top: windowTopPos[id],
+            left: windowLeftPos[id],
+          },
+          200
+        );
       }
 
       function setupInteractions() {
-        var mobile = isMobile();
+        var isMobile = window.innerWidth <= 768;
 
         try {
           $("#mSafrain .window").draggable("destroy");
@@ -111,37 +87,21 @@
           $("#mSafrain .wincontent").resizable("destroy");
         } catch (e) {}
 
-        if (!mobile) {
+        if (!isMobile) {
           $("#mSafrain .wincontent").resizable();
           $("#mSafrain .window").draggable({ cancel: ".wincontent" });
         }
-      }
-
-      // MOBILE: open + scroll to window
-      function mobileFocusWindow(id) {
-        var $win = $("#window" + id);
-        if (!$win.length) return;
-
-        $win.removeClass("closed minimizedWindow").show();
-        $("#minimPanel" + id)
-          .removeClass("closed minimizedTab")
-          .addClass("activeTab");
-
-        var offsetTop = $win.offset().top;
-        $("html, body").animate({ scrollTop: offsetTop - 80 }, 300);
       }
 
       // INITIALISE WINDOWS
       $("#mSafrain .window").each(function () {
         $(this).css("z-index", 1000);
         $(this).attr("data-id", i);
-
         minimizedWidth[i] = $(this).width();
         minimizedHeight[i] = $(this).height();
         windowTopPos[i] = $(this).css("top");
         windowLeftPos[i] = $(this).css("left");
 
-        // Taskbar tab
         $("#taskbar").append(
           '<div class="taskbarPanel" id="minimPanel' +
             i +
@@ -167,7 +127,6 @@
         );
       });
 
-      // Last window active by default
       $("#minimPanel" + (i - 1)).addClass("activeTab");
       $("#window" + (i - 1)).addClass("activeWindow");
 
@@ -176,9 +135,7 @@
 
       // EVENTS
       $("#mSafrain .window").mousedown(function () {
-        if (!isMobile()) {
-          makeWindowActive($(this).attr("data-id"));
-        }
+        makeWindowActive($(this).attr("data-id"));
       });
 
       $("#mSafrain").on("click", ".winclose", function () {
@@ -186,18 +143,11 @@
       });
 
       $("#mSafrain").on("click", ".winminimize", function () {
-        var id = $(this).parent().parent().attr("data-id");
-        minimizeWindow(id);
+        minimizeWindow($(this).parent().parent().attr("data-id"));
       });
 
-      // Taskbar click
       $("#mSafrain").on("click", ".taskbarPanel", function () {
         id = $(this).attr("data-id");
-
-        if (isMobile()) {
-          mobileFocusWindow(id);
-          return;
-        }
 
         if ($(this).hasClass("activeTab")) {
           minimizeWindow(id);
@@ -208,31 +158,26 @@
         }
       });
 
-      // TOP LAUNCHBAR (works anywhere in DOM)
-      $(document).on("click", ".openWindow", function () {
-        var id = $(this).attr("data-id");
-        if (typeof id === "undefined") return;
-
-        if (isMobile()) {
-          mobileFocusWindow(id);
-        } else {
-          openWindow(id);
-        }
+      $("#mSafrain").on("click", ".openWindow", function () {
+        openWindow($(this).attr("data-id"));
       });
 
       $("#mSafrain").on("click", ".winmaximize", function () {
         var win = $(this).parent().parent();
-        var wid = win.attr("data-id");
         if (win.hasClass("fullSizeWindow")) {
           win.removeClass("fullSizeWindow");
           win
             .find(".wincontent")
-            .height(minimizedHeight[wid])
-            .width(minimizedWidth[wid]);
+            .height(minimizedHeight[win.attr("data-id")])
+            .width(minimizedWidth[win.attr("data-id")]);
         } else {
           win.addClass("fullSizeWindow");
-          minimizedHeight[wid] = win.find(".wincontent").height();
-          minimizedWidth[wid] = win.find(".wincontent").width();
+          minimizedHeight[win.attr("data-id")] = win
+            .find(".wincontent")
+            .height();
+          minimizedWidth[win.attr("data-id")] = win
+            .find(".wincontent")
+            .width();
           adjustFullScreenSize();
         }
       });
